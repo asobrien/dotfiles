1214c304723c9a60af570ebbcff475b6c3687da1
Anthony O'Brien <asobrien@jwplayer.com>
2016-06-06 21:29:55 -0400

    Adds "gg" alias for git graphs

--- slimzsh/aliases.zsh
+++ slimzsh/aliases.zsh
@@ -1,21 +1,19 @@
 # UTILITY
-
 alias less='less -R'
 alias grep='grep --color=auto'
 alias ..='cd ../'
 
 # GIT
-
-alias gd='git diff'
-alias gco='git checkout'
-alias gs='git status'
-alias gl='git pull'
-alias gp='git push'
-alias gpp='git pull; git push'
+# alias gd='git diff'
+# alias gco='git checkout'
+# alias gs='git status'
+# alias gl='git pull'
+# alias gp='git push'
+# alias gpp='git pull; git push'
 alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
+alias gg='git log --oneline --abbrev-commit --all --graph --decorate --color'
 
 # SOURCE CUSTOM ALIASES
-
 if [[ -f $slim_path/aliases.zsh.local ]]; then
   source $slim_path/aliases.zsh.local
 fi
