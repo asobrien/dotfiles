24e62df295e0843aa1d606dd497a8437c560b252
Anthony O'Brien <asobrien@jwplayer.com>
2016-09-02 10:12:28 -0400

    source the submods

--- slimzsh/plugins/submodule.zsh
+++ slimzsh/plugins/submodule.zsh
@@ -1,20 +1,21 @@
 # initialize a submodule if required
-return 1
-# check if submodules need initing
-submodstats=$(cd ${__dotfiles_absdir} && git submodule status --recursive)
+# return 1
+# # check if submodules need initing
+# submodstats=$(cd ${__dotfiles_absdir} && git submodule status --recursive)
 
-for submodstat in $submodstats; do
-    if [[ ${submodstat:0:1} = \- ]] ; then
-        echo
-        echo "Initializing and updating submodules ..."
-        echo
-        # run in a subshell
-        # TODO: there is probably a better way, but this works
-        /bin/bash -c "cd ${__dotfiles_absdir} && git submodule update --init --recursive"
-        break
-    fi
-done
+# for submodstat in $submodstats; do
+#     if [[ ${submodstat:0:1} = \- ]] ; then
+#         echo
+#         echo "Initializing and updating submodules ..."
+#         echo
+#         # run in a subshell
+#         # TODO: there is probably a better way, but this works
+#         /bin/bash -c "cd ${__dotfiles_absdir} && git submodule update --init --recursive"
+#         break
+#     fi
+# done
 
+# these should be initted since we use git subtrees
 # load submods
 source $slim_path/submod/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source $slim_path/submod/zsh-history-substring-search/zsh-history-substring-search.zsh
