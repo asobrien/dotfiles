6847b979d460a59ffeb691e53312292dc04550fe
Anthony O'Brien <asobrien@jwplayer.com>
2017-01-03 13:29:07 -0500

    reorganized; adds release notes

--- src/tmp/provisioning.sh
+++ src/tmp/provisioning.sh
@@ -5,44 +5,81 @@
 # This is host specific
 REPO_SALT="${HOME}/Code/jwp/provisioning"
 REPO_PILLAR="${HOME}/Code/jwp/pillar"
-GH_USER="asobrien"
-function gf_fea_start {
-        feature_name="$GH_USER/$1"
-        git checkout develop && \
-        git fetch origin && \
-        git pull --rebase=preserve origin develop && \
-        git checkout -b feature/$feature_name;
-}
+#GH_USER="asobrien"
 
-function gf_fea_prov_finish {
-        feature_name="matt/$1"
-        git checkout develop && \
-        git fetch origin && \
-        git pull --rebase=preserve origin develop && \
-        git checkout feature/$feature_name && \
-        git pull --rebase=preserve origin feature/$feature_name
-        git push origin -u feature/$feature_name && \
-        git rebase develop && \
-        git push origin feature/$feature_name --force-with-lease && \
-        git pull-request -b develop
-        git checkout develop
-}
+# function gf_fea_start {
+#         feature_name="$GH_USER/$1"
+#         git checkout develop && \
+#         git fetch origin && \
+#         git pull --rebase=preserve origin develop && \
+#         git checkout -b feature/$feature_name;
+# }
 
-function gf_fea_pill_finish {
-        feature_name="matt/$1"
-        git checkout develop && \
-        git fetch origin && \
-        git pull --rebase=preserve origin develop && \
-        git checkout feature/$feature_name && \
-        git pull --rebase=preserve origin feature/$feature_name
-        git push origin -u feature/$feature_name  && \
-        git rebase develop && \
-        git push origin feature/$feature_name --force-with-lease && \
-        git checkout develop && \
-        git merge --no-ff feature/$feature_name  && \
-        git pull --rebase=preserve origin develop  && \
-        git push origin develop
-}
+# function gf_fea_prov_finish {
+#         feature_name="matt/$1"
+#         git checkout develop && \
+#         git fetch origin && \
+#         git pull --rebase=preserve origin develop && \
+#         git checkout feature/$feature_name && \
+#         git pull --rebase=preserve origin feature/$feature_name
+#         git push origin -u feature/$feature_name && \
+#         git rebase develop && \
+#         git push origin feature/$feature_name --force-with-lease && \
+#         git pull-request -b develop
+#         git checkout develop
+# }
+
+# function gf_fea_pill_finish {
+#         feature_name="matt/$1"
+#         git checkout develop && \
+#         git fetch origin && \
+#         git pull --rebase=preserve origin develop && \
+#         git checkout feature/$feature_name && \
+#         git pull --rebase=preserve origin feature/$feature_name
+#         git push origin -u feature/$feature_name  && \
+#         git rebase develop && \
+#         git push origin feature/$feature_name --force-with-lease && \
+#         git checkout develop && \
+#         git merge --no-ff feature/$feature_name  && \
+#         git pull --rebase=preserve origin develop  && \
+#         git push origin develop
+# }
+
+
+# function gf_hotfix_start {
+#         hotfix_date=`date +"%Y.%m.%d"`
+#         hotfix_name="$hotfix_date.$1"
+#         echo $hotfix_name
+#         git checkout master && \
+#         git fetch origin && \
+#         git pull --rebase=preserve origin master && \
+#         git checkout -b hotfix/$hotfix_name && \
+#         git push origin -u hotfix/$hotfix_name
+# }
+
+# function gf_hotfix_finish {
+#         hotfix_date=`date +"%Y.%m.%d"`
+#         hotfix_name="$hotfix_date.$1"
+#         echo $hotfix_name
+#         git fetch origin && \
+#         git checkout hotfix/$hotfix_name && \git
+#         git pull --rebase=preserve origin hotfix/$hotfix_name &&\
+#         git pull --rebase=preserve origin master &&\
+#         git checkout master && \
+#         git pull --ff-only origin master && \
+#         git merge --no-ff hotfix/$hotfix_name && \
+#         git push origin master && \
+#         git tag -a $hotfix_name && \
+#         git push --tags origin && \
+#         git fetch origin && \
+#         git checkout develop && \
+#         git pull --rebase=preserve origin develop && \
+#         git pull --rebase=preserve origin master && \
+#         git push origin develop --force-with-lease
+# }
+
+
+### THIS IS WHAT WE USE
 
 # USED THIS IS NICE !!!
 function gf_rel_start {
@@ -82,37 +119,14 @@ function gf_rel_finish {
         git push origin develop --force-with-lease
 }
 
-function gf_hotfix_start {
-        hotfix_date=`date +"%Y.%m.%d"`
-        hotfix_name="$hotfix_date.$1"
-        echo $hotfix_name
-        git checkout master && \
-        git fetch origin && \
-        git pull --rebase=preserve origin master && \
-        git checkout -b hotfix/$hotfix_name && \
-        git push origin -u hotfix/$hotfix_name
+# something similar could be done for pillar with "Merge branch"
+function gf_rel_notes {
+        local last_tag
+        last_tag=`git tag | grep "^[0-9]\{4\}\.[0-9]\{2\}\.[0-9]\{2\}\..*$" | sort -n | tail -n 1`
+        git log --grep "Merge pull request" "$last_tag"..HEAD --pretty="* %s&&>>  %b" \
+                | sed "s/Merge pull request/PR/g" | sed "s/from .*&&>> //g"
 }
 
-function gf_hotfix_finish {
-        hotfix_date=`date +"%Y.%m.%d"`
-        hotfix_name="$hotfix_date.$1"
-        echo $hotfix_name
-        git fetch origin && \
-        git checkout hotfix/$hotfix_name && \git
-        git pull --rebase=preserve origin hotfix/$hotfix_name &&\
-        git pull --rebase=preserve origin master &&\
-        git checkout master && \
-        git pull --ff-only origin master && \
-        git merge --no-ff hotfix/$hotfix_name && \
-        git push origin master && \
-        git tag -a $hotfix_name && \
-        git push --tags origin && \
-        git fetch origin && \
-        git checkout develop && \
-        git pull --rebase=preserve origin develop && \
-        git pull --rebase=preserve origin master && \
-        git push origin develop --force-with-lease
-}
 
 
 # RELEASE BOTH
