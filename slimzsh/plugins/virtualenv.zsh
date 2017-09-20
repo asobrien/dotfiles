6e52537a0fc37daede99f57fbbdd7f42c330e815
Anthony O'Brien <asobrien@jwplayer.com>
2017-09-20 12:38:41 -0400

    remove old slimzsh

--- slimzsh/plugins/virtualenv.zsh
+++
@@ -1,8 +0,0 @@
-# load virtualenvwrapper, if installed
-
-if [[ -f "/usr/local/bin/virtualenvwrapper.sh" ]]; then
-    export WORKON_HOME=$HOME/.virtualenvs
-    export PROJECT_HOME=$HOME/Code
-    export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
-    source /usr/local/bin/virtualenvwrapper_lazy.sh
-fi
